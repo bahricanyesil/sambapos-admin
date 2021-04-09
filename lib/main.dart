2bb04e06a532e1cab4510a5166db87ee8faf0e9f
Bahrican Yesil <bahricanyesil@gmail.com>
2021-04-09 15:22:41 +0300

    master

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SambaPOS Admin Panel',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
